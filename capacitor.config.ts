fac8c2b3dbd075ef4cecf7c41b22765685cd5632
joansj21 <joansj21@gmail.com>
2024-05-31 22:12:46 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FrontCliente',
+  webDir: 'www'
+};
+
+export default config;
